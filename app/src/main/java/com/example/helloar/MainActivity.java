2a09acd33efd89d1748ad98be3ae60423435502f
sarvani29 <sarvani009@gmail.com>
2020-04-01 05:28:24 +0530

    Sceneform Execution

---
+++ app/src/main/java/com/example/helloar/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.helloar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+
+
+//reference to arFragment under activity_main.xml
+
+//ArPlace is created. Cam be horizontal/vertical
+
+        //anchor is created. Describes a fixed location and orientation in the real world.
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor(); //render 3D model on top of this anchor
+
+            //create model, add model to scene
+            ModelRenderable.builder()
+                    .setSource(this, Uri.parse("ArcticFoc_Posed.sfb.sfb"))
+                    .build()
+                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
+                    .exceptionally(throwable -> {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setMessage(throwable.getMessage())
+                                .show();
+                        return null;
+                    });
+
+        });
+
+    }
+//tranformable to change the size of the anchornode as it cannot resize by itself
+
+    private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor); //created from hitresult
+        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+        transformableNode.setParent(anchorNode);
+        transformableNode.setRenderable(modelRenderable);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        transformableNode.select();
+    }
+}
